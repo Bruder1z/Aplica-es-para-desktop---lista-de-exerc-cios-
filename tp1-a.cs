8c05177548f3d0911aeb4ed3928e6b38756e0e03
Lucas Bruder <110999721+Bruder1z@users.noreply.github.com>
2024-04-24 13:57:13 -0300

    Add files via upload

---
+++ tp1-a.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float n1, n2, n3, n4, media;
+            n1 = float.Parse(textBox1.Text);
+            n2 = float.Parse(textBox2.Text);
+            n3 = float.Parse(textBox3.Text);
+            n4 = float.Parse(textBox4.Text);
+            media = (n1 + n2 + n3 + n4) / 4;
+            MessageBox.Show("Média = " + media);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
---
+++ tp1-b.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int angulo;
+            angulo = int.Parse(textBox1.Text);
+            double medGrau = angulo * (Math.PI / 180);
+
+            Math.Sin(medGrau);
+            Math.Cos(medGrau);
+            Math.Tan(medGrau);
+            double secante = 1 /Math.Cos(medGrau);
+            MessageBox.Show("Sen = " + Math.Sin(medGrau));
+            MessageBox.Show("Cos = " + Math.Cos(medGrau));
+            MessageBox.Show("Tan = " + Math.Tan(medGrau));
+            MessageBox.Show("Sec = " + secante);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
